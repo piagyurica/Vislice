3b1b6a2bac3d0f6b5dc7334f167a3281e5272590
Pia Gyurica <piakaufmann@Pias-MacBook-Pro-2.local>
2020-05-05 15:24:35 +0200

    Dodala drugo opcijo za izračun praštevil

--- test.py
+++ test.py
@@ -12,3 +12,14 @@ for i in range(4, 200):
 
     if je_prastevilo:
         print(i)
+
+# for i in range(1, 201):
+    # skip = False
+    # j = 2
+    # while j ** 2 <= i:
+        # if i % j == 0:
+            # skip = True
+            # break
+        # j += 1
+    # if not skip:
+        # print(i)
